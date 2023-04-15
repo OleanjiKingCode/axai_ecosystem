550da838c7b5e48806ef837271722b5b955c48d5
OleanjiKingCode <holamilean56@gmail.com>
2023-04-15 23:00:24 +0100

    home page done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import "../styles/globals.css";
 import { configureChains, Connector, createClient, WagmiConfig } from "wagmi";
 import {
-	mainnet,
-	polygon,
-	optimism,
-	arbitrum,
-	goerli,
-	polygonMumbai,
-	optimismGoerli,
-	arbitrumGoerli,
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  goerli,
+  polygonMumbai,
+  optimismGoerli,
+  arbitrumGoerli,
 } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
@@ -21,48 +21,48 @@ import { Roboto } from "next/font/google";
 import { StrictMode } from "react";
 
 const roboto = Roboto({
-	subsets: ["latin"],
-	weight: ["700"],
-	display: "swap",
+  subsets: ["latin"],
+  weight: ["700"],
+  display: "swap",
 });
 
 const { chains, provider } = configureChains(
-	[
-		mainnet,
-		goerli,
-		polygon,
-		polygonMumbai,
-		optimism,
-		optimismGoerli,
-		arbitrum,
-		arbitrumGoerli,
-	],
-	[alchemyProvider({ apiKey: config.Alchemy_Api_Key }), publicProvider()],
+  [
+    mainnet,
+    goerli,
+    polygon,
+    polygonMumbai,
+    optimism,
+    optimismGoerli,
+    arbitrum,
+    arbitrumGoerli,
+  ],
+  [alchemyProvider({ apiKey: config.Alchemy_Api_Key }), publicProvider()]
 );
 
 export const connectors: Connector[] = [
-	new MetaMaskConnector({ chains, options: { shimDisconnect: true } }),
+  new MetaMaskConnector({ chains, options: { shimDisconnect: true } }),
 ];
 
 const wagmiClient = createClient({
-	autoConnect: true,
-	connectors,
-	provider,
+  autoConnect: true,
+  connectors,
+  provider,
 });
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	return (
-		<chakra.div className={roboto.className}>
-			<ChakraProvider resetCSS>
-				<WagmiConfig client={wagmiClient}>
-					<chakra.div w="full" minH="100vh" bg="#17171a">
-						<Navbar />
-						<Component {...pageProps} />
-					</chakra.div>
-				</WagmiConfig>
-			</ChakraProvider>
-		</chakra.div>
-	);
+  return (
+    <chakra.div className={roboto.className}>
+      <ChakraProvider resetCSS>
+        <WagmiConfig client={wagmiClient}>
+          <chakra.div w="full" minH="100vh" bg="#17171a" color="white">
+            <Navbar />
+            <Component {...pageProps} />
+          </chakra.div>
+        </WagmiConfig>
+      </ChakraProvider>
+    </chakra.div>
+  );
 };
 
 export default MyApp;
