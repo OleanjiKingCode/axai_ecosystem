30a09f257de7471a31364c97cb760eb6b293af50
OleanjiKingCode <holamilean56@gmail.com>
2023-04-21 22:12:49 +0100

    Done with upload to lens

--- src/pages/articles/new.tsx
+++ src/pages/articles/new.tsx
@@ -1,43 +1,44 @@
 import {
-	Box,
-	Flex,
-	Heading,
-	Tag,
-	chakra,
-	VStack,
-	HStack,
-	Icon,
-	TagLabel,
-	Text,
-	Image,
-	useToast,
-	Input,
-	Grid,
-	GridItem,
-	Button,
-	ListItem,
-	OrderedList,
-	Circle,
-	Textarea,
-	List,
-	ListIcon,
-	Popover,
-	PopoverTrigger,
-	PopoverContent,
-	PopoverBody,
-	PopoverArrow,
-	useDisclosure,
+  Box,
+  Flex,
+  Heading,
+  Tag,
+  chakra,
+  VStack,
+  HStack,
+  Icon,
+  TagLabel,
+  Text,
+  Image,
+  useToast,
+  Input,
+  Grid,
+  GridItem,
+  Button,
+  ListItem,
+  OrderedList,
+  Circle,
+  Textarea,
+  List,
+  ListIcon,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  PopoverArrow,
+  useDisclosure,
+  Spinner,
 } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import {
-	RiThumbUpLine,
-	RiThumbUpFill,
-	RiMessage3Line,
-	RiBookmarkFill,
-	RiFileCopyFill,
-	RiShareLine,
-	RiBookmarkLine,
-	RiMoreLine,
+  RiThumbUpLine,
+  RiThumbUpFill,
+  RiMessage3Line,
+  RiBookmarkFill,
+  RiFileCopyFill,
+  RiShareLine,
+  RiBookmarkLine,
+  RiMoreLine,
 } from "react-icons/ri";
 import Link from "next/link";
 import { useCreatePost } from "@/lib/useCreatePost";
@@ -45,185 +46,188 @@ import { LENS_CONTRACT_ADDRESS, LENS_CONTRACT_ABI } from "@/const/contracts";
 import { ethers, Signer } from "ethers";
 
 const NewArticle = () => {
-	const [viewing, setViewing] = useState<string>("");
-	const [value, setValue] = useState("");
+  const [viewing, setViewing] = useState<string>("");
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState<File | null>(null);
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [content, setContent] = useState<string>("");
 
-	const [image, setImage] = useState<File | null>(null);
-	const [title, setTitle] = useState<string>("");
-	const [description, setDescription] = useState<string>("");
-	const [content, setContent] = useState<string>("");
+  const { mutateAsync: createPost } = useCreatePost();
+  // rome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const handleInputChange = (e: any) => {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  };
+  // rome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const OnFileChange = (e: any) => {
+    try {
+      const file = e.target.files[0];
+      setImage(file);
+      if (file) {
+        const image = URL.createObjectURL(file);
+        setViewing(image);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const { mutateAsync: createPost } = useCreatePost();
-	// rome-ignore lint/suspicious/noExplicitAny: <explanation>
-	const handleInputChange = (e: any) => {
-		const inputValue = e.target.value;
-		setValue(inputValue);
-	};
-	// rome-ignore lint/suspicious/noExplicitAny: <explanation>
-	const OnFileChange = (e: any) => {
-		try {
-			const file = e.target.files[0];
-			setImage(file);
-			if (file) {
-				const image = URL.createObjectURL(file);
-				setViewing(image);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const createNew = async () => {
+    setLoading(true);
+    if (!image) return;
+    const tx = await createPost({
+      image,
+      title,
+      description,
+      content,
+    });
+    setLoading(false);
+  };
 
-	return (
-		<Flex w="full" gap="2" direction="column" py="7">
-			<Text px="5" color="#efd8af" fontSize="3xl" w="full" textAlign="end">
-				New Article
-			</Text>
+  return (
+    <Flex w="full" gap="2" direction="column" py="7">
+      <Text px="5" color="#efd8af" fontSize="3xl" w="full" textAlign="end">
+        New Article
+      </Text>
 
-			<Grid h="full" templateColumns="repeat(8, 1fr)" py="5" gap={4} px="4">
-				<GridItem
-					colSpan={2}
-					py="8"
-					px="5"
-					h="fit-content"
-					bg="gray.700"
-					rounded="xl"
-				>
-					<VStack w="fit" gap={6}>
-						<Flex alignItems="center" justifyContent="center">
-							<Flex
-								w={56}
-								h={36}
-								bg={viewing ? "transparent" : "gray.300"}
-								rounded="lg"
-								color="black"
-								justifyContent="center"
-								alignItems="center"
-							>
-								{!viewing ? (
-									<Text>IMAGE HERE</Text>
-								) : (
-									<Image src={viewing} alt="Your Image" w="70%" />
-								)}
-							</Flex>
-						</Flex>
+      <Grid h="full" templateColumns="repeat(8, 1fr)" py="5" gap={4} px="4">
+        <GridItem
+          colSpan={2}
+          py="8"
+          px="5"
+          h="fit-content"
+          bg="gray.700"
+          rounded="xl"
+        >
+          <VStack w="fit" gap={6}>
+            <Flex alignItems="center" justifyContent="center">
+              <Flex
+                w={56}
+                h={36}
+                bg={viewing ? "transparent" : "gray.300"}
+                rounded="lg"
+                color="black"
+                justifyContent="center"
+                alignItems="center"
+              >
+                {!viewing ? (
+                  <Text>IMAGE HERE</Text>
+                ) : (
+                  <Image src={viewing} alt="Your Image" w="70%" />
+                )}
+              </Flex>
+            </Flex>
 
-						<VStack w="full">
-							<Text w="full">File:</Text>
-							<Input
-								type="file"
-								variant="outline"
-								onChange={OnFileChange}
-								py={1}
-								px={3}
-								borderWidth={1} // adds border
-								borderColor="gray.300" // sets border color
-								borderRadius="md" // sets border radius
-								_hover={{ borderColor: "gray.400" }} // changes border color on hover
-								_focus={{ outline: "none", boxShadow: "outline" }} // removes default focus styling and adds custom box shadow
-								accept="image/*" // restricts file selection to image files
-							/>
-						</VStack>
-						<VStack w="full">
-							<Text w="full">Summary:</Text>
-							<Textarea
-								w="full"
-								value={value}
-								onChange={handleInputChange}
-								rows={7}
-							/>
-						</VStack>
-					</VStack>
-				</GridItem>
-				<GridItem
-					colSpan={{ base: 6, md: 4 }}
-					py="8"
-					px="5"
-					bg="gray.700"
-					rounded="xl"
-					h="fit-content"
-					justifyContent="start"
-				>
-					<VStack gap="5" w="full" justifyContent="start">
-						<Input
-							type="text"
-							variant="outline"
-							placeholder="Title"
-							fontSize="2xl"
-							py={4}
-							px={3}
-							border="none"
-							borderRadius="md"
-							onChange={(e) => setTitle(e.target.value)}
-						/>
+            <VStack w="full">
+              <Text w="full">File:</Text>
+              <Input
+                type="file"
+                variant="outline"
+                onChange={OnFileChange}
+                py={1}
+                px={3}
+                borderWidth={1} // adds border
+                borderColor="gray.300" // sets border color
+                borderRadius="md" // sets border radius
+                _hover={{ borderColor: "gray.400" }} // changes border color on hover
+                _focus={{ outline: "none", boxShadow: "outline" }} // removes default focus styling and adds custom box shadow
+                accept="image/*" // restricts file selection to image files
+              />
+            </VStack>
+            <VStack w="full">
+              <Text w="full">Summary:</Text>
+              <Textarea
+                w="full"
+                value={value}
+                onChange={handleInputChange}
+                rows={7}
+              />
+            </VStack>
+          </VStack>
+        </GridItem>
+        <GridItem
+          colSpan={{ base: 6, md: 4 }}
+          py="8"
+          px="5"
+          bg="gray.700"
+          rounded="xl"
+          h="fit-content"
+          justifyContent="start"
+        >
+          <VStack gap="5" w="full" justifyContent="start">
+            <Input
+              type="text"
+              variant="outline"
+              placeholder="Title"
+              fontSize="2xl"
+              py={4}
+              px={3}
+              border="none"
+              borderRadius="md"
+              onChange={(e) => setTitle(e.target.value)}
+            />
 
-						<Input
-							type="text"
-							variant="outline"
-							placeholder="Highlight in a sentence"
-							fontSize="md"
-							py={4}
-							px={3}
-							borderWidth={1}
-							borderColor="gray.300"
-							borderRadius="md"
-							border="none"
-							onChange={(e) => setDescription(e.target.value)}
-						/>
+            <Input
+              type="text"
+              variant="outline"
+              placeholder="Highlight in a sentence"
+              fontSize="md"
+              py={4}
+              px={3}
+              borderWidth={1}
+              borderColor="gray.300"
+              borderRadius="md"
+              border="none"
+              onChange={(e) => setDescription(e.target.value)}
+            />
 
-						<Textarea
-							w="full"
-							placeholder="In the year 3248, the world had become a post-apocalyptic..."
-							rows={17}
-							onChange={(e) => setContent(e.target.value)}
-						/>
-						<Button
-							onClick={async () => {
-								if (!image) return;
-
-								return await createPost({
-									image,
-									title,
-									description,
-									content,
-								});
-							}}
-							bg="gray.600"
-							fontWeight="500"
-							_hover={{ bg: "gray.800" }}
-						>
-							Create Post
-						</Button>
-					</VStack>
-				</GridItem>
-				<GridItem
-					colSpan={{ md: 2 }}
-					display={{ base: "none", md: "unset" }}
-					minHeight="200px"
-					h="fit-content"
-					py="8"
-					px="5"
-					bg="gray.700"
-					rounded="xl"
-					position="sticky"
-				>
-					<VStack w="full" px="3" gap="2" position="sticky">
-						<Text>From your Page</Text>
-						<Flex
-							direction="column"
-							w="full"
-							alignItems="center"
-							justifyContent="center"
-							gap="3"
-						>
-							<Box w={56} h={56} rounded="xl" bg="gray.300" />
-							<Box w={56} h={56} rounded="xl" bg="gray.300" />
-							<Box w={56} h={56} rounded="xl" bg="gray.300" />
-						</Flex>
-					</VStack>
-				</GridItem>
-			</Grid>
-		</Flex>
-	);
+            <Textarea
+              w="full"
+              placeholder="In the year 3248, the world had become a post-apocalyptic..."
+              rows={17}
+              onChange={(e) => setContent(e.target.value)}
+            />
+            <Button
+              onClick={createNew}
+              bg="gray.600"
+              fontWeight="500"
+              _hover={{ bg: "gray.800" }}
+            >
+              {loading ? <Spinner /> : "Create Post"}
+            </Button>
+          </VStack>
+        </GridItem>
+        <GridItem
+          colSpan={{ md: 2 }}
+          display={{ base: "none", md: "unset" }}
+          minHeight="200px"
+          h="fit-content"
+          py="8"
+          px="5"
+          bg="gray.700"
+          rounded="xl"
+          position="sticky"
+        >
+          <VStack w="full" px="3" gap="2" position="sticky">
+            <Text>From your Page</Text>
+            <Flex
+              direction="column"
+              w="full"
+              alignItems="center"
+              justifyContent="center"
+              gap="3"
+            >
+              <Box w={56} h={56} rounded="xl" bg="gray.300" />
+              <Box w={56} h={56} rounded="xl" bg="gray.300" />
+              <Box w={56} h={56} rounded="xl" bg="gray.300" />
+            </Flex>
+          </VStack>
+        </GridItem>
+      </Grid>
+    </Flex>
+  );
 };
 
 export default NewArticle;
