d40513e0f0f8cfb39b3e8a00668533be12c2d9c6
OleanjiKingCode <holamilean56@gmail.com>
2023-05-11 23:38:49 +0100

    URL

--- src/Data/config.ts
+++ src/Data/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   BASE_URL: "localhost:3000",
   SiteUrlLink:
-    process.env.NEXT_PUBLIC_SITE_URL || "https://axai-ecosystem.vercel.app/",
+    process.env.NEXT_PUBLIC_SITE_URL || "https://axai-ecosystem.vercel.app",
 };
